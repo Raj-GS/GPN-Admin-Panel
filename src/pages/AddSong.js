8eabcf2c1615387237956db2813ba438691b81db
SanthoshiNaar <santoshi@naarsoft.com>
2025-08-19 16:26:44 +0530

    ckeditor issue

--- src/pages/AddSong.js
+++ src/pages/AddSong.js
@@ -10,8 +10,7 @@ import {
   Radio,FormControl, Select,
   MenuItem,InputLabel,ListItemText, Checkbox, Grid
 } from "@mui/material";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
 
 
 
